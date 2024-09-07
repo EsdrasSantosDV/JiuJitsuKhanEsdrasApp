f9d96e79f28d27e7ab2ba8645b4a6c48904ce131
Esdras Santos <esdrassantos@MacBook-Pro-de-Esdras.local>
2024-09-07 17:44:18 -0300

    feat:initial repo

---
+++ JiuJitsuEsdrasKhan/JiuJitsuEsdrasKhanApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiuJitsuEsdrasKhanApp.swift
+//  JiuJitsuEsdrasKhan
+//
+//  Created by Esdras Santos on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct JiuJitsuEsdrasKhanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
